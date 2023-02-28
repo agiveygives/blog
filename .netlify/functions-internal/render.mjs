3fd0349f422f985e727c155918fef07335d31acc
agiveygives <justincase25xxv@gmail.com>
2023-02-27 23:06:28 -0600

    use netlify adapter

--- .netlify/functions-internal/render.mjs
+++ .netlify/functions-internal/render.mjs
@@ -1,4 +1,6 @@
-export const manifest = {
+import { init } from '../serverless.js';
+
+export const handler = init({
 	appDir: "_app",
 	appPath: "_app",
 	assets: new Set(["andrew.png","global.css","images/bitmoji-bird-friends.png","images/css-logo.png","images/docker-logo.png","images/graphql-logo.png","images/react-logo.png","images/ruby-on-rails-logo.png","images/typescript-logo.svg"]),
@@ -6,12 +8,12 @@ export const manifest = {
 	_: {
 		client: {"start":{"file":"_app/immutable/entry/start.f5921a93.js","imports":["_app/immutable/entry/start.f5921a93.js","_app/immutable/chunks/index.82666dd5.js","_app/immutable/chunks/singletons.22e8f2cb.js"],"stylesheets":[],"fonts":[]},"app":{"file":"_app/immutable/entry/app.e6ce4b91.js","imports":["_app/immutable/entry/app.e6ce4b91.js","_app/immutable/chunks/index.82666dd5.js"],"stylesheets":[],"fonts":[]}},
 		nodes: [
-			() => import('./nodes/0.js'),
-			() => import('./nodes/1.js'),
-			() => import('./nodes/2.js'),
-			() => import('./nodes/3.js'),
-			() => import('./nodes/4.js'),
-			() => import('./nodes/5.js')
+			() => import('../server/nodes/0.js'),
+			() => import('../server/nodes/1.js'),
+			() => import('../server/nodes/2.js'),
+			() => import('../server/nodes/3.js'),
+			() => import('../server/nodes/4.js'),
+			() => import('../server/nodes/5.js')
 		],
 		routes: [
 			{
@@ -48,4 +50,4 @@ export const manifest = {
 			return {  };
 		}
 	}
-};
+});
--- .svelte-kit/output/client/_app/immutable/chunks/1.588b816d.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.989be764.js";export{t as component};
