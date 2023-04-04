5f4fcc83f73a352e4abce8f2982cd7d10654a646
Andrew <justincase25xxv@gmail.com>
2023-04-04 17:17:05 -0500

    use uuidv4 for blog ids (#8)

--- src/routes/api/blog/+server.ts
+++ src/routes/api/blog/+server.ts
@@ -1,8 +1,8 @@
 import { json, error, type RequestHandler } from '@sveltejs/kit';
-import { collection, query, orderBy, getDocs, setDoc, doc } from 'firebase/firestore';
+import { setDoc, doc } from 'firebase/firestore';
+import { v4 as uuidv4 } from 'uuid';
 import { db } from '@/firebase.config';
 
-/** @type {import('./$types').RequestHandler} */
 export const POST: RequestHandler = async ({ request }) => {
 	let newBlogId = '';
 	const now = new Date().toUTCString();
@@ -10,11 +10,7 @@ export const POST: RequestHandler = async ({ request }) => {
 	try {
 		const data = await request.json();
 
-		const blogsRef = collection(db, 'blogs');
-		const q = query(blogsRef, orderBy('createdAt', 'desc'));
-		const blogsData = await getDocs(q);
-
-		newBlogId = `${blogsData.size + 1}`;
+		newBlogId = uuidv4();
 
 		await setDoc(doc(db, 'blogs', newBlogId), {
 			...data,
