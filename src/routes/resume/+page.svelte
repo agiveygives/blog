7a2be4bd6bdcb456aa9ddf6b47d524cf6a011523
agiveygives <JustinCase25XXV@gmail.com>
2023-04-12 17:44:35 -0500

    start making resume page

---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,22 @@
+<div class="page-content">
+	<article>
+		<section>
+			<h1>Andrew Givens</h1>
+		</section>
+
+		<aside>
+			<p>Raytown, MO</p>
+		</aside>
+	</article>
+</div>
+
+<style>
+	article {
+		display: grid;
+		grid-template-columns: 75% 25%;
+	}
+
+	h1 {
+		font-size: 28px;
+	}
+</style>
