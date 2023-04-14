ae99bc6333b10838a5438885c1be9c01cefd1373
agiveygives <JustinCase25XXV@gmail.com>
2023-04-14 08:27:23 -0500

    build resume with html

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -2,21 +2,272 @@
 	<article>
 		<section>
 			<h1>Andrew Givens</h1>
+			<div class="section-divider" />
+
+			<div class="content-section">
+				<h2>Professional Summary</h2>
+				<div class="section-divider" />
+
+				<p>
+					Hardworking software development professional driven to increase team effectiveness. Focused on usability and performance improvements. Proven history of developing useful, efficient and cost-effective projects.
+				</p>
+			</div>
+
+			<div class="content-section">
+				<h2>Work History</h2>
+				<div class="section-divider" />
+
+				<div>
+					<p>
+						<strong>Senior Software Engineer</strong>, 10/2021 - Current
+						<br/>
+						<strong>Big Nerd Ranch</strong>, Atlanta, GA (Remote)
+					</p>
+					<ul>
+						<li>Worked with team members to stand up a new project with Postgres, Ruby on Rails, GraphQL, React, TypeScript, and Docker.</li>
+						<li>Integrated and wrote tests with jest, react-testing-library, RSpec, and Cypress.</li>
+						<li>Integrated with Stripe to enable international payments between users.</li>
+						<li>Integrated with the client's existing Ruby on Rails application.</li>
+						<li>Implemented authentication and authorization within the Rails API and React front-end.</li>
+						<li>Debugged any issues as they came up and collaborated with other engineers to determine efficient fixes.</li>
+						<li>Documented architecture patterns and debugging techniques for future and current engineers.</li>
+						<li>Participated in architecture designs and discussions for features.</li>
+						<li>Participated in UX design discussions with the client's designer.</li>
+						<li>Collaborated with the client team to construct an agile culture that fit best for their needs.</li>
+						<li>Collaborated with clients to define product requirements.</li>
+						<li>Updated job knowledge by studying state-of-the-art development tools and programming techniques and participating in educational opportunities.</li>
+						<li>Integrated third-party tools and components into applications.</li>
+						<li>Led software development initiative as subject matter expert and primary point-of-contact for project management staff.</li>
+					</ul>
+				</div>
+
+				<div>
+					<p>
+						<strong>Front-End Software Engineer</strong>, 07/2020 - 10/2021
+						<br/>
+						<strong>Retail Success</strong>, Overland Park, KS (Hybrid)
+					</p>
+					<ul>
+						<li>Developed for an ASP.NET application using TypeScript, React and Redux.</li>
+						<li>Pioneered a shift to test-driven-development within our front-end applications using react-testing-library.</li>
+						<li>Uplifted our ASP.NET MVC front-end to a mobile friendly React application.</li>
+						<li>Developed technical solutions required to accommodate specific user-facing assets.</li>
+						<li>Translated UX and business requirements into elegant code solutions.</li>
+						<li>Integrated enhancements into web design to improve user experience, smooth functionality, and boost load times.</li>
+					</ul>
+				</div>
+
+				<div>
+					<p>
+						<strong>Software Engineer</strong>, 07/2018 - 07/2020
+						<br/>
+						<strong>Cerner</strong>, Kansas City, MO
+					</p>
+					<ul>
+						<li>Developed for a Ruby on Rails application using React and Redux.</li>
+						<li>Developed for an API using Ruby and AWS Technologies.</li>
+						<li>Developed enhancements and bug fixes for web components using Javascript.</li>
+						<li>Developed tests with Rspec, Jest with Enzyme, Jasmine, and Selenium WebdriverIO.</li>
+						<li>Worked with software development and testing team members to design and develop robust solutions to meet client requirements for functionality, scalability, and performance.</li>
+						<li>Provided guidance and mentored less-experienced staff members.</li>
+						<li>Informed internal personnel and external stakeholders of project milestones.</li>
+						<li>Partnered with development team on product development and application support plans.</li>
+					</ul>
+				</div>
+
+				<div>
+					<p>
+						<strong>Software Engineer Intern</strong>, 05/2017 - 07/2017
+						<br/>
+						<strong>Cerner</strong>, Kansas City, MO
+					</p>
+					<ul>
+						<li>Used Javascript, HTML, and CSS to complete stories during two week iterations.</li>
+						<li>Authored code fixes and enhancements for inclusion in future code releases and patches.</li>
+						<li>Estimated work hours and tracked progress using Scrum methodology.</li>
+						<li>Collaborated with engineers to split the code base to improve maintainability.</li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="content-section">
+				<h2>Education</h2>
+				<div class="section-divider" />
+
+				<p>
+					<strong>Bachelor of Science, Computer Science</strong>, 05/2018
+					<br/>
+					<strong>Missouri University of Science and Technology</strong> - Rolla, MO
+				</p>
+		</div>
 		</section>
 
 		<aside>
-			<p>Raytown, MO</p>
+			<div class="info-line">
+				<svg x="0px" y="0px" viewBox="-0.5 -0.5 19 19" class="icon">
+					<circle id="Oval-Copy-5" fill="#FFFFFF" cx="9" cy="9" r="9"></circle>
+					<g id="Group-3" class="fillclr" transform="translate(5.000000, 4.000000)">
+					<path fill="#02bca3" d="M3.7,5.1C2.8,5.1,2,4.4,2,3.5s0.7-1.6,1.6-1.6s1.6,0.7,1.6,1.6S4.6,5.1,3.7,5.1 M3.7,0
+									C1.6,0,0,1.6,0,3.7c0,0.8,0.6,2.3,1.7,4.2c0.8,1.5,1.7,2.7,1.7,2.7L3.7,11l0.2-0.3c0,0,0.9-1.3,1.7-2.7c1.1-2,1.7-3.4,1.7-4.2
+									C7.3,1.6,5.7,0,3.7,0"></path>
+					</g>
+				</svg>
+				<p>Raytown, MO</p>
+			</div>
+
+			<div class="info-line">
+				<svg viewBox="-0.5 -0.5 19 19" class="icon">
+					<g id="ACQD68_v1_icons" transform="translate(-32.000000, -253.000000)">
+							<g id="Group-10" transform="translate(32.000000, 253.000000)">
+									<circle id="Oval-Copy-3" fill="#FFFFFF" cx="9" cy="9" r="9"></circle>
+									<g id="Group-3" fill="#02bca3" transform="translate(4.500000, 4.000000)">
+											<path d="M8.945125,7.7507875 C8.872625,8.1107875 8.760125,8.4045375 8.598875,8.6507875 C8.426375,8.9095375 8.203875,9.1120375 7.917625,9.2695375 C7.525125,9.4857875 7.097625,9.5945375 6.641375,9.5945375 C6.298875,9.5945375 5.938875,9.5332875 5.562625,9.4120375 C4.803875,9.1670375 4.076375,8.7070375 3.527625,8.3320375 C2.997625,7.9707875 2.511375,7.5182875 2.040125,6.9520375 C1.540125,6.4107875 1.157625,5.8682875 0.868875,5.2957875 C0.568875,4.7032875 0.207625,3.9207875 0.065125,3.1370375 C-0.099875,2.2295375 0.051375,1.4507875 0.515125,0.8207875 C0.708875,0.5570375 0.940125,0.3645375 1.218875,0.2270375 C1.482625,0.0995375 1.788875,0.0257875 2.156375,0.0020375 C2.370125,-0.0117125 2.661375,0.0470375 2.828875,0.4082875 C2.963875,0.6995375 3.106375,0.9945375 3.242625,1.2795375 C3.342625,1.4845375 3.443875,1.6957875 3.542625,1.9057875 C3.606375,2.0407875 3.658875,2.1770375 3.700125,2.3095375 C3.780125,2.5732875 3.707625,2.7895375 3.477625,2.9720375 C3.181375,3.2057875 2.891375,3.4357875 2.616375,3.6695375 C2.605125,3.6795375 2.582625,3.6982875 2.568875,3.7120375 C2.572625,3.7257875 2.581375,3.7532875 2.600125,3.7970375 C2.881375,4.4920375 3.225125,5.0620375 3.647625,5.5370375 L3.653875,5.5445375 C4.071375,6.0257875 4.590125,6.4407875 5.241375,6.8120375 C5.282625,6.8370375 5.307625,6.8482875 5.322625,6.8532875 C5.337625,6.8407875 5.358875,6.8232875 5.370125,6.8132875 C5.637625,6.5695375 5.902625,6.3132875 6.175125,6.0507875 C6.385125,5.8470375 6.610125,5.8020375 6.860125,5.9170375 C6.987625,5.9745375 7.113875,6.0457875 7.240125,6.1257875 C7.433875,6.2520375 7.630125,6.3807875 7.821375,6.5057875 C8.086375,6.6795375 8.358875,6.8582875 8.630125,7.0307875 C8.967625,7.2445375 8.987625,7.5395375 8.945125,7.7507875" id="Fill-1" mask="url(#mask-2)"></path>
+									</g>
+							</g>
+					</g>
+				</svg>
+				<p>(417) 880 - 1846</p>
+			</div>
+
+			<div class="info-line">
+				<svg viewBox="-0.5 -0.5 19 19" class="icon">
+					<g transform="translate(-32.000000, -373.000000)">
+								<g id="Group-7" transform="translate(32.000000, 373.000000)">
+										<circle id="Oval-Copy-7" fill="#FFFFFF" cx="9" cy="9" r="9"></circle>
+										<path fill="#02bca3" d="M4.0425,6.40875 L4.0425,11.95 L6.8125,9.18 L4.0425,6.40875 Z M9.07375,10.46875 L13.54375,6 L4.45375,6 L8.92375,10.46875 C8.965,10.51 9.0325,10.51 9.07375,10.46875 L9.07375,10.46875 Z M9.20125,11.16375 C9.1475,11.21875 9.075,11.2475 8.99875,11.2475 C8.9225,11.2475 8.85125,11.21875 8.7975,11.16375 L7.22375,9.59 L4.47,12.34375 L13.53,12.34375 L10.77625,9.58875 L9.20125,11.16375 Z M13.9575,6.4075 L13.9575,11.94875 L11.18625,9.17875 L13.9575,6.4075 Z" id="Fill-1"></path>
+								</g>
+					</g>
+				</svg>
+				<p>agivens1996@gmail.com</p>
+			</div>
+
+			<div class="info-line">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="icon">
+					<g>
+						<circle fill="#ffffff" cx="256" cy="256" r="256" />
+						<!--! Font Awesome Pro 6.4.0 by @fontawesome  https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+						<path class="fa-icon" fill="#02bca3" d="M64 480H448c35.3 0 64-28.7 64-64V160c0-35.3-28.7-64-64-64H288c-10.1 0-19.6-4.7-25.6-12.8L243.2 57.6C231.1 41.5 212.1 32 192 32H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64z"/>
+					</g>
+					</svg>
+				<p>andrewgivens.net</p>
+			</div>
+
+			<div>
+				<h2 class="white">Profiles</h2>
+				<div class="section-divider white" />
+				<p>linkedin.com/in/andrew-givens-464291127</p>
+				<p>github.com/agiveygives</p>
+			</div>
+
+			<div>
+				<h2 class="white">Skills</h2>
+				<div class="section-divider white" />
+
+				<ul>
+					<li>React</li>
+					<li>NodeJS</li>
+					<li>TypeScript</li>
+					<li>CSS and Sass</li>
+					<li>Ruby on Rails</li>
+					<li>GraphQL</li>
+					<li>REST APIs</li>
+					<li>Unit Testing</li>
+					<li>Relational Databases</li>
+					<li>Svelte and Sveltekit</li>
+					<li>React Native</li>
+				</ul>
+			</div>
 		</aside>
 	</article>
 </div>
 
 <style>
+	.white {
+		color: white;
+	}
+
+	.page-content {
+		align-items: center;
+	}
+
+	.fa-icon {
+		transform: scale(0.5) translate(256px, 256px);
+	}
+
+	.icon {
+		height: 19px;
+    width: 20px;
+    vertical-align: middle;
+	}
+
 	article {
 		display: grid;
 		grid-template-columns: 75% 25%;
+		background-color: white;
+		border-radius: 5px;
+		flex: 1;
+		max-width: 900px;
+
+		--primary: #02bca3;
+	}
+
+	.section-divider {
+		height: 1px;
+		background-color: var(--primary);
+		width: 100%;
+	}
+
+	aside .section-divider {
+		background-color: white;
+		margin-left: -10px;
+	}
+
+	section, aside {
+		padding: 10px 30px;
+	}
+
+	.content-section {
+		margin: 20px 0px;
+	}
+
+	aside {
+		background-color: var(--primary);
+		border-top-right-radius: 5px;
+		border-bottom-right-radius: 5px;
+		color: white;
+		padding-left: 10px;
+	}
+
+	h1, h2 {
+		text-transform: uppercase;
+		color: var(--primary);
 	}
 
 	h1 {
-		font-size: 28px;
+		font-size: 42px;
+		font-weight: 900;
+		margin: 10px 0px;
+	}
+
+	h2 {
+		font-size: 18px;
+		font-weight: 600;
+		margin: 0px;
+		letter-spacing: 2px;
+	}
+
+	p, ul {
+		font-size: 14px;
+	}
+
+	li {
+		padding-top: 3px;
+	}
+
+	.info-line {
+		display: grid;
+    grid-template-columns: min-content auto;
+    align-items: center;
+    gap: 8px;
 	}
 </style>
